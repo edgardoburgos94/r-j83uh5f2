427caea425af57fba67603541836620d083e1bfb
Edgardo David <edgardo.burgos94@gmail.com>
2018-03-04 16:04:33 -0500

    Update counter everytime the text change

--- src/App.js
+++ src/App.js
@@ -2,13 +2,25 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+  };
   render() {
     return (
       <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
+        <textarea rows="3" onChange={this.countLetters.bind(this)}></textarea>
+        <div className="counter">{this.state.count}</div>
       </div>
     );
+  };
+
+  countLetters() {
+    this.setState({
+      count: this.state.count + 1
+    });
   }
 }
 
